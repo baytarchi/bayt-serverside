445a1062a0e1e9bf26c9c3618c99203b7aacbc05
dkn1ght23 <mujammal.salman@gmail.com>
2024-06-03 22:37:43 +0600

    comment removed

--- services/application/applicationService.js
+++ services/application/applicationService.js
@@ -7,13 +7,6 @@ const contactUs = async (name, email, description) => {
   try {
     const subject = `Contacted by: ${email}`;
     const body = mailerBody.getMailBody(email, subject, description);
-
-    // const body = `<p>Hi ${name}</p>
-    //                               <br>Welcome abord! Your user creation our application is successful<br>
-    //                               <a href="http://localhost:5173/${token}">Verify Email</a><br>
-    //                               <br>Cheers<br>`;
-    // const body = description;
-
     await mailService.sendMail(subject, body);
   } catch (e) {
     response.message = "Technical Error: Unable to send contact";
