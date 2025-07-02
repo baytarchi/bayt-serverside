e898689edca8ccdd5f0eb118f8933c8c11334fcb
dkn1ght23 <mujammal.salman@gmail.com>
2025-07-02 17:26:21 -0400

    middle ware fixed

--- routes/project/index.js
+++ routes/project/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const projectController = require("../../controllers/project/projectController");
+const upload = multer();
 
-router.post("/", projectController.addProject);
-router.patch("/:project_id", projectController.updateProject);
+router.post("/", upload.none(), projectController.addProject);
+router.patch("/:project_id", upload.none(), projectController.updateProject);
 router.get("/", projectController.getProjects);
 router.get("/:project_slug", projectController.getProject);
 router.delete("/:project_id", projectController.deleteProject);
